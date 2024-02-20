1233d9b8ed59be8551a606ece065ccbffe2c6b01
jasveerhashtrust <jasveer@hashtrust.in>
2024-02-20 14:17:26 +0530

    juhyg

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import markdown
-import requests
+import os
+from dotenv import load_dotenv
 
 def extract_md_content(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -8,27 +9,12 @@ def extract_md_content(file_path):
         return html_content
 
 def create_product_in_stripe(html_content):
-    stripe_secret_key = 'your_stripe_secret_key_here'
-    endpoint = 'https://api.stripe.com/v1/products'
-
-    headers = {
-        'Authorization': f'Bearer {stripe_secret_key}',
-        'Content-Type': 'application/x-www-form-urlencoded',
-    }
-
-    data = {
-        'name': 'Your Product Name',
-        'description': html_content,
-        'type': 'service',
-    }
-
-    print(html_content)
-
-    # response = requests.post(endpoint, headers=headers, data=data)
-    return response.json()
+    load_dotenv()
+    stripe_secret_key = os.getenv('API_KEY')
+    print(stripe_secret_key)
 
 if __name__ == '__main__':
     file_path = './blog.md'
     extracted_content = extract_md_content(file_path)
     response = create_product_in_stripe(extracted_content)
-    # print(response)
+    print(response)
